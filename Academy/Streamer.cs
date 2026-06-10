7b707eb7e17a581dcb13beb8d6cc84860b146851
I_Swerk_I <I_Swerk_I@DESKTOP-1LE6UQ2>
2026-06-10 19:32:33 +1000

    Streamer DONE

---
+++ Academy/Streamer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Academy
+{
+    internal class Streamer
+    {
+        public void Save(Human[] group, string filename)
+        {
+            Directory.SetCurrentDirectory($"{Application.ExecutablePath}\\..\\..\\..");
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            //string filename = "group.csv";
+            StreamWriter writer = new StreamWriter(filename);
+            foreach (Human h in group)
+            {
+                writer.WriteLine(h.ToFileSring());
+            }
+            writer.Close();
+            Process.Start("notepad", filename);
+        }
+
+        public void Print(Human[] group)
+        {
+            foreach (Human h in group)
+            {
+                Console.WriteLine(h);
+            }
+        }
+
+        public Human[] Load(string filename)
+        {
+            List<Human> group = new List<Human>();
+            Directory.SetCurrentDirectory($"{Application.ExecutablePath}\\..\\..\\..");
+            StreamReader reader = new StreamReader(filename);
+            while (!reader.EndOfStream)
+            {
+                string record = reader.ReadLine();
+                Console.WriteLine(record);
+                group.Add(Factory.Create(record.Split(':').First()).Init(record.Split(":,;".ToCharArray())));
+            }
+
+            reader.Close();
+            return group.ToArray();
+        }
+    }
+
+}
